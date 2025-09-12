0fd32cbc79502b1fe58811aaecd8989d05d5a89d
nahueelm19 <nahuelm15@hotmail.com>
2025-09-12 10:01:58 -0300

    mensaje de alerta del div

--- eventos.js
+++ eventos.js
@@ -1,4 +1,4 @@
-let contenedor = document.getElementsByClassName('div');
-contenedor.addEventListener('click', function() {
+let container = document.getElementById('contenedor');
+container.addEventListener('click', function() {
     alert('Hola! Soy el div');
 })
